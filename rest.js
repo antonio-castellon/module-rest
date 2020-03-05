0bbfb529e6a600d27b7863134590b0dff75cc044
Castellon, Antonio (contracted) <Antonio.Castellon@contracted.pmi.com>
2020-03-05 14:04:52 +0100

    move the endpoint to the root path

--- rest.js
+++ rest.js
@@ -76,7 +76,7 @@ module.exports = function(_SERVER, _AUTH, api) {
         res.status(500).json({ERROR: err});
     })
 
-    app.use('c', function(req, res){
+    app.use('/', function(req, res){
 
         let routes = [];
 
